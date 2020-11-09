541d66a32927d7c53c9b9ede458a4cb4c08b828e
Ting <0917355950teresa@gmail.com>
2020-11-09 17:00:01 +0800

    create board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,9 +9,26 @@ class BoardsController < ApplicationController
   end
 
   def new
+    @board = Board.new
   end
 
   def create
-    redirect_to "/"
+    # 用create方法（不常用）
+    # @board = Board.create(params["board"])
+    # if @board
+    # else
+    # end
+
+    clean_params = params.require(:board).permit(:title)
+    @board = Board.new(clean_params)
+    # @board.title = params['board']['title'] =>若只有一欄可不用洗乾淨
+
+    if @board.save
+      # flash["notice"] = "成功新增看板"
+      redirect_to "/", notice: '成功新增看板'
+    else
+      render :new  # =render 'board/new'
+    end
+
   end
 end
