cdad52c8b0da527a54b1ac11efc4f80bd1dac5e9
Ting <0917355950teresa@gmail.com>
2020-12-04 16:35:27 +0800

    like功能-@rails/ujs-ajax

--- app/frontend/controllers/favorite_controller.js
+++ app/frontend/controllers/favorite_controller.js
@@ -8,7 +8,8 @@
 // </div>
 
 import { Controller } from "stimulus"
-import ax from 'axios'
+// import ax from 'axios'
+import magicRails from '@rails/ujs'
 
 export default class extends Controller {
   static targets = [ "icon" ]
@@ -16,23 +17,42 @@ export default class extends Controller {
   go(e){
     e.preventDefault();
     const id =this.data.get('id');
-    const token = document.querySelector('[name=csrf-token]').content
-    ax.defaults.headers.common['X-CSRF-TOKEN'] = token
 
-    ax.post(`/posts/${id}/favorite`, {})
-      .then((resp) => {
-        if (resp.data.status == "added") {
+    magicRails.ajax({
+      url: `/posts/${id}/favorite`,
+      type: 'post',
+      success: (resp) => {
+        if (resp.status == "added") {
           this.iconTarget.classList.add('fas')
           this.iconTarget.classList.remove('far')      
         } else {
           this.iconTarget.classList.add('far')
           this.iconTarget.classList.remove('fas')      
         }
-        console.log(resp.data);
-      })
-      .catch(function(err) {
-        console.log(err);
-      })
+        console.log(resp.status);
+      },
+      error: function(err) {
+        console.log(err)
+      }
+    })
+
+    // const token = document.querySelector('[name=csrf-token]').content
+    // ax.defaults.headers.common['X-CSRF-TOKEN'] = token
+
+    // ax.post(`/posts/${id}/favorite`, {})
+    //   .then((resp) => {
+    //     if (resp.data.status == "added") {
+    //       this.iconTarget.classList.add('fas')
+    //       this.iconTarget.classList.remove('far')      
+    //     } else {
+    //       this.iconTarget.classList.add('far')
+    //       this.iconTarget.classList.remove('fas')      
+    //     }
+    //     console.log(resp.data);
+    //   })
+    //   .catch(function(err) {
+    //     console.log(err);
+    //   })
 
   }
 }
