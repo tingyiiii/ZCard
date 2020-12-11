c1135153faab247ffc496122403ea5b17bcfc107
Ting <0917355950teresa@gmail.com>
2020-12-11 17:29:11 +0800

    metaprogramming

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -2,4 +2,20 @@ module BoardsHelper
   def board_title_css(board)
     "board-title board-title-#{board.state}"
   end
+
+  # def check_open?(board)
+  #   board.may_open? && policy(board).open?
+  # end
+
+  # def check_lock?(board)
+  #   board.may_lock? && policy(board).lock?
+  # end
+
+  # def check_hide?(board)
+  #   board.may_hide? && policy(board).hide?
+  # end
+
+  def check?(obj, type)
+    obj.send("may_#{type}?") && policy(obj).send("#{type}?")
+  end
 end
