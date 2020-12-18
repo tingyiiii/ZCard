7ff193040a01a6288b48bdf9a6d2fa7e9ed34fce
Ting <0917355950teresa@gmail.com>
2020-12-18 12:53:38 +0800

    購物車頁面

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,6 +7,14 @@ class CartsController < ApplicationController
 
     redirect_to pricing_path, notice: '已加入購物車'
   end
+
+  def show
+  end
+
+  def destroy
+    session[:cart9999] = nil
+    redirect_to pricing_path, notice: '購物車已清除！'
+  end
   
   private
   def current_cart
